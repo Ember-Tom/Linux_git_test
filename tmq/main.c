72eeacabee11bc5800283790ce1d1e0f7776c4b2
Ember <EmberSTM@outlook.com>
2025-09-14 17:32:23 +0800

    Compile and run on Windows 25 09 14

--- tmq/main.c
+++ tmq/main.c
@@ -2,7 +2,12 @@
 
 int main(void)
 {
-    printf("Hello World!");
+    int a=0,b=7,c=0;
+    int *p = &a;
+    *p=15;
+    c = a + b;
+    printf("a + b = %d + %d = %d\n", a, b, c);
+    //printf("Hello World!");
     return 0;
 }
 
